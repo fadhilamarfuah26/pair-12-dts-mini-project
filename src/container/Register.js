c247576ff9249e9b54013303f840e14c6fff46f9
fadhilamarfuah26 <marfuahfadhila26@gmail.com>
2022-07-20 13:54:25 -0700

    name

--- src/container/Register.js
+++ src/container/Register.js
@@ -49,6 +49,16 @@ const Register = () => {
                 </Typography>
                 <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
                     <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="name"
+                                label="Your name"
+                                name="name"
+                                autoComplete="name"
+                            />
+                        </Grid>
                         <Grid item xs={12}>
                             <TextField
                                 required
